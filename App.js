2b8efa681e0f679649ff1f752f4253e3c44287d4
CasttCK <vini.kronemberger@gmail.com>
2021-12-06 13:48:09 -0300

    Criação do projeto

---
+++ App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Feed from './src/Telas/Feed';
+
+import {SafeAreaView} from 'react-native';
+
+function App() {
+  return (
+    <SafeAreaView>
+      <Feed></Feed>
+    </SafeAreaView>
+  );
+}
+
+export default App;
---
+++ src/Telas/Feed/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+
+function Feed(){
+    return(
+        <View>
+        <Text>
+            Hello World !!
+        </Text>
+        </View>
+    )
+}
+
+export default Feed;
