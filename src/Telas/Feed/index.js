ae91bbfb6c23c5f833f0f8abc6cc035a90a281f2
CasttCK <vini.kronemberger@gmail.com>
2021-12-07 14:22:33 -0300

    Preenchimento da página Feed

--- src/Telas/Feed/index.js
+++ src/Telas/Feed/index.js
@@ -1,17 +1,47 @@
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import {View, Text, FlatList} from 'react-native';
+import styles from './style';
+import {View, Text, FlatList, Image} from 'react-native';
+
+import Header from '../../Components/Header/header';
 
 function Feed() {
-    const posts = [
-        {
-            id: '1',
-        },
-    ];
 
-    function renderItem(){
-        return <Text>Hello</Text>;
-    }
+  const posts = [
+    {
+      id: '1',
+      user: 'Dolores Fuertes de Barriga',
+      place: 'Disney',
+      pictureUrl: 'https://maladeaventuras.com/wp-content/uploads/2019/03/IMG_5801-1.jpg',
+      likesCount: '',
+      descricao: '',
+      hashtags: '',
+    },
+  ];
+
+  function renderItem({item: post}){
+      return (
+        <View>
+          <View style={styles.appHeader}>
+            <Header />
+          </View>
+            <View style={styles.postHeader}>
+              <View >
+                <Text style={styles.infoUser}>{post.user}</Text>
+              </View>
+              <View style={styles.postPlace}>
+                <Text style={styles.infoPlace}>{post.place}</Text>
+              </View>
+            </View>
+            <View>
+              <Image 
+               source= {{uri: post.pictureUrl}}
+                style= {styles.userPicture}
+              />
+            </View>
+        </View>
+      );
+  }
 
   return (
     <View>
---
+++ src/Telas/Feed/style.js
@@ -0,0 +1,35 @@
+/* eslint-disable prettier/prettier */
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+
+    postHeader:{
+    
+    },
+  
+    infoUser:{
+      fontSize: 15,
+      fontWeight: 'bold',
+      color: 'black',
+    },
+  
+    postPlace:{
+  
+    },
+  
+    infoPlace:{
+      fontSize: 14,
+    },
+  
+    userPicture:{
+      width: '100%',
+      height: 400,
+    },
+  
+    appHeader:{
+      
+    },
+
+});
+
+export default styles;
