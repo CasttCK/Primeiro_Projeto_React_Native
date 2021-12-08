4d6d46f6fa7109915e22c0d6bb86b41766f7e1d8
CasttCK <vini.kronemberger@gmail.com>
2021-12-08 14:24:59 -0300

    Adição de dependências do Java

--- android/app/src/main/java/com/primeiroprojeto/MainActivity.java
+++ android/app/src/main/java/com/primeiroprojeto/MainActivity.java
@@ -1,6 +1,7 @@
 package com.primeiroprojeto;
 
 import com.facebook.react.ReactActivity;
+import android.os.Bundle;
 
 public class MainActivity extends ReactActivity {
 
@@ -12,4 +13,9 @@ public class MainActivity extends ReactActivity {
   protected String getMainComponentName() {
     return "PrimeiroProjeto";
   }
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+  super.onCreate(null);
+}
 }
